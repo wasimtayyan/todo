42d8cd1c6706f83bcff34cd94c6389c3c14f8368
wasim altayyan <wasimtayyan@gmail.com>
2023-06-23 18:06:21 +0300

    change BG color

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
 } from "firebase/firestore";
 
 const style = {
-  bg: `h-screen w-s p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`,
+  bg: `h-screen w-s p-4 bg-gradient-to-r from-[#4f72da] to-[#da79c6]`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shaow-xl p-4`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2 `,
   form: `flex justify-between `,
